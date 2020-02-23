c3b260ac6b6fa656eae72d3088b5ef2db5ade494
Tomas <ts@medei.dk>
2020-02-23 20:06:06 +0100

    Setting | Profile component done with settigns item component

---
+++ client/src/components/Common/SettingItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SettingItem() {
+    return (
+        <div class="align-center row margin-top-s">
+            <img class="settings-icon" src="https://via.placeholder.com/32" />
+            <div>Setting Item name</div>
+        </div>
+    )
+}
--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import MainNavbar from '../Common/MainNavbar';
+import SettingItem from '../Common/SettingItem';
 
 export default class Profile extends Component {
     render() {
         return (
-            <div>
-                Profile
+             <div class="container">
+                <div class="row">
+                    <div class="side-col grey-bg full-height">
+                        <div class="row shadow">
+                            <div class="col padding-32">
+                                <MainNavbar />
+                                <div class="row">
+                                    <h1 class="margin-sm">Settings</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row padding-32">
+                            <div class="col"> 
+                                <div class="row justify-center padding-20">
+                            <img class="setting-profile-image" src="https://via.placeholder.com/160" />
+                            </div>
+                            <div class="row justify-center">
+                                <h2>Username</h2>
+                            </div>
+                            <div class="row">
+                                <h4>Setting group name</h4>
+                            </div>
+                            <SettingItem />
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col full-width full-height blue-bg">
+                         
+                    </div>
+                </div>
             </div>
         )
     }
