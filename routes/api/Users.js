e3e84efe644634f49b3d2741810262c19daa13bd
jose <josecarlosesparza@gmail.com>
2020-02-24 12:52:50 +0100

    Create token authentication system

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+let jwt = require('jsonwebtoken');
+let config = require('../../config/tokenConfig');
+let middleware = require('../../middleware/checkToken');
 
 //User Model
 const User = require('../../models/User');
@@ -23,14 +28,76 @@ router.get('/:id', (req, res) => {
 
 // POST
 // Register new user
-router.post('/register', (req, res) => {
-    // TODO: Work with auth
+router.post('/register', async (req, res) => {
+    // Encrypt the password
+    // First create the salt, then the hash with the password and the salt as parameters
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
+    // If there are no errors: save the user into the database and redirect them to '/login'
+    try {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+        });
+
+        newUser.save();
+        res.status(200).send("Welcome to the club!");
+
+    } catch (err) {
+        // If there are errors: send an error
+        res.status(500)
+            .send("Error registering new user please try again.");
+    }
+
 });
 
 // POST
 // Login with user credentials
 router.post('/login', (req, res) => {
-    // TODO: Work with auth
+    let password = req.body.password;
+    let email = req.body.email;
+
+    if (password && email) {
+
+        User.findOne({ "email": email }, function (err, foundedUser) {
+            if (!foundedUser) {
+                res.json({
+                    success: false,
+                    message: 'Incorrect credentials 1'
+                });
+            } else {
+                bcrypt.compare(password, foundedUser.password, (err, password) => {
+                    if (password) {
+                        let token = jwt.sign({ email: email },
+                            config.secret,
+                            {
+                                expiresIn: '24h' // expires in 24 hours
+                            }
+                        );
+                        // return the JWT token for the future API calls
+                        res.json({
+                            success: true,
+                            message: 'Authentication successful!',
+                            token: token
+                        });
+                    } else {
+                        res.json({
+                            success: false,
+                            message: 'Incorrect credentials 2'
+                        });
+                    }
+                })
+            }
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'Authentication failed! Please check the request'
+        });
+    }
 });
 
 // TODO: Decide on how to handle the "logout" functionality
