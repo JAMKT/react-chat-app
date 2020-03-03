01d637bc8a86c45bb23818eaf1d69cb005e590d4
jose <josecarlosesparza@gmail.com>
2020-03-03 17:23:04 +0100

    Testing in progress

--- client/src/components/Common/ContactList.js
+++ client/src/components/Common/ContactList.js
@@ -3,6 +3,7 @@ import UserListItem from './UserListItem';
 import UserListGroup from './UserListGroup';
 import AlphabeticalSlider from './AlphabeticalSlider';
 import getCurrentUser from '../../currentUser';
+import axios from 'axios';
 
 export default function ContactList(props) {
     getCurrentUser();
