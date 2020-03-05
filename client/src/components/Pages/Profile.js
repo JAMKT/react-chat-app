8420ebf18e96dbe8d80195fe080308675219bbe5
Alessia Trianti <alestrianti@gmail.com>
2020-03-05 11:30:39 +0100

    User settings: functional

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -5,38 +5,29 @@ import { AuthContext } from '../context/authContext';
 
 
 const Profile = () => {
-        const auth = useContext(AuthContext);
+    const auth = useContext(AuthContext);
 
-        return (
-          
-             <div className="container">
-                 {console.log('this is the current user from the profile page')}
-                 {console.log(auth.currUser)}
-                 {auth.loggedIn && <div>logged in!</div>}
-                 {!auth.loggedIn && <div>not logged in!</div>}
+    return (
+            <div className="container">
+            <div className="row">
+                <div className="side-col grey-bg full-height">
+                    <div className="row shadow">
+                        <div className="col padding-32">
+                            <MainNavbar />
 
-                <div className="row">
-                    <div className="side-col grey-bg full-height">
-                        <div className="row shadow">
-                            <div className="col padding-32">
-                                <MainNavbar />
-
-                                <div className="row">
-                                    <h1 className="margin-sm">Profile</h1>
-                                </div>
+                            <div className="row">
+                                <h1 className="margin-sm">Profile</h1>
                             </div>
                         </div>
-                        <SettingItem />
-                        
-                        
                     </div>
-                    <div className="col full-width full-height blue-bg">
-                         
-                    </div>
+                    <SettingItem auth={auth.currUser} />
+                </div>
+                <div className="col full-width full-height blue-bg">
+                        
                 </div>
             </div>
-        )
-    
+        </div>
+    )
 }
 
 export default Profile;
