30e1ecee81072c9fc26e3ba42994e111fb2857c2
jose <josecarlosesparza@gmail.com>
2020-03-09 18:03:28 +0100

    Divide messages by user in current chat

--- client/src/components/Common/MainMessageChat.js
+++ client/src/components/Common/MainMessageChat.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import ChatHeader from './ChatHeader';
 import ResponseMessage from './ResponseMessage';
 import Message from './Message';
@@ -24,8 +24,6 @@ const MainMessageChat = (props) => {
 
     const sendMessage = (event) => {
         event.preventDefault();
-
-
         const data = {
             content: document.getElementById("message").value,
             author: {
@@ -33,7 +31,7 @@ const MainMessageChat = (props) => {
                 username: auth.currUser.username
             }
         }
-        
+
         document.getElementById("message").value = "";
 
         const config = {
@@ -48,12 +46,38 @@ const MainMessageChat = (props) => {
         axios.post('/api/chats/' + chatId + '/messages', data, config)
             .then((newMessage) => {
                 if (newMessage) {
-                    console.log("Full Message");
                     console.log(newMessage);
                 }
             })
             .catch(err => console.log(err));
     }
+
+    const getMessages = () => {
+        const chatId = props.chat._id;
+
+        axios.get('/api/chats/' + chatId + '/messages')
+            .then((messages) => {
+                messages.data.messages.forEach((message) => {
+                    if (message.author.id === auth.currUser._id) {
+                        //TODO
+                        console.log("By Current User:")
+                        console.log(message.content);
+                    } else {
+                        //TODO
+                        console.log("By " + message.author.username);
+                        console.log(message.content);
+                    }
+
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        if (props.chat) {
+            getMessages();
+        }
+    });
 
     if (props.chat !== null) {
         return (
@@ -64,9 +88,6 @@ const MainMessageChat = (props) => {
                     <ResponseMessage />
                     <ResponseMessage />
                     <Message />
-                </div>
-                <div>
-                    {props.chat.author.username}
                 </div>
                 <form className="padding-16 chat-input-field-position" onSubmit={sendMessage}>
                     <input type="message" id="message" className="chat-input-field" placeholder="Type your message..." onInput={inputHandler} />
