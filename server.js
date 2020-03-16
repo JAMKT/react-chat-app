001a430fc54e2e264859b287ccd1b43cc818f549
jose <josecarlosesparza@gmail.com>
2020-03-16 18:26:01 +0100

    Try to make it run

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
 //Serve static assets if we are in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build')); 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
