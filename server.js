8b4639a90b30e8e125dc15cf7ee353a8bfcf87d3
jose <josecarlosesparza@gmail.com>
2020-03-16 18:05:32 +0100

    Heroku test

--- server.js
+++ server.js
@@ -57,7 +57,6 @@ app.use((req, res, next) => {
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
     app.use(express.static('client/build'));
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
