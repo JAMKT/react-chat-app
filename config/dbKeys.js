1a47707719fb8378d328f7f8c4aca45ebfce5976
jose <josecarlosesparza@gmail.com>
2020-03-16 17:47:06 +0100

    Git ignore change

---
+++ config/dbKeys.js
@@ -0,0 +1,6 @@
+if(process.env.NODE_ENV === 'production') {
+    module.exports = { mongoURI: 'mongodb+srv://admin:dbAdmin2020@cluster0-g7y2f.mongodb.net/react-chat-app?retryWrites=true&w=majority' }; // Add your remote db connection string here
+} else {
+    // module.exports = { mongoURI: 'mongodb://localhost:27017/react_chat_app' }; // Add your local db connection string here
+    module.exports = { mongoURI: 'mongodb+srv://admin:dbAdmin2020@cluster0-g7y2f.mongodb.net/react-chat-app-dev?retryWrites=true&w=majority' }; // Add your local db connection string here
+}
---
+++ config/sessionConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: 'g00dN3ws'
+}
