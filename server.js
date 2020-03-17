adb1e1c2f1b9baba6cf908867a5f9cf683668876
jose <josecarlosesparza@gmail.com>
2020-03-17 21:00:00 +0100

    Ready for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require("passport");
 const session = require("express-session");
 const sessionSecret = require('./config/sessionConfig').secret;
+const path = require('path');
 
 const users = require('./routes/api/Users');
 const chats = require('./routes/api/Chats');
@@ -62,44 +63,52 @@ const Chat = require('./models/Chat');
 const Message = require('./models/Message');
 // Socket.io connection
 
-    io.on('connection', socket => {
-        socket.on('send-message', packet => {
-            try {
-                const newMessage = new Message({
-                    content: packet.data.content,
-                    author: {
-                        id: packet.data.author.id,
-                        username: packet.data.author.username
+io.on('connection', socket => {
+    socket.on('send-message', packet => {
+        try {
+            const newMessage = new Message({
+                content: packet.data.content,
+                author: {
+                    id: packet.data.author.id,
+                    username: packet.data.author.username
+                }
+            });
+
+            Chat.findById(packet.chatId, (err, chat) => {
+                if (err) console.log('Message not found.');
+
+                Message.create(newMessage, (err, message) => {
+                    if (err) {
+                        console.log(err);
+                        console.log("Message was not created...");
+                    } else {
+                        message.save();
+                        chat.messages.push(message);
+                        chat.lastUpdate = message.created;
+                        chat.save();
                     }
                 });
-
-                Chat.findById(packet.chatId, (err, chat) => {
-                    if (err) console.log('Message not found.');
-
-                    Message.create(newMessage, (err, message) => {
-                        if (err) {
-                            console.log(err);
-                            console.log("Message was not created...");
-                        } else {
-                            message.save();
-                            chat.messages.push(message);
-                            chat.lastUpdate = message.created;
-                            chat.save();
-                        }
-                    });
-                }).then(chat => {
-                    socket.emit('get-messages', "Message");
-                }).catch(err => console.log(err));
-            } catch (err) {
-                console.log('Could not create this message.');
-            }
-        });
+            }).then(chat => {
+                socket.emit('get-messages', "Message");
+            }).catch(err => console.log(err));
+        } catch (err) {
+            console.log('Could not create this message.');
+        }
     });
+});
 
 
 io.on('disconnect', () => {
     console.log('Disconnected.');
 });
+
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = require('./config/env').serverPORT;
 server.listen(port, () => console.log(`Server started on port ${port}`));
